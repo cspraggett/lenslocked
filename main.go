859b83ea32becb760393fe35b15f9069befcfa5b
cspraggett <cspraggett@gmail.com>
2020-03-03 21:45:43 -0500

    change route in main to use users new function

--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 
 	// "github.com/julienschmidt/httprouter"
 	"github.com/gorilla/mux"
+	"lenslocked.com/controllers"
 	"lenslocked.com/views"
 )
 
-var homeView, contactView, faqView, signupView *views.View
+var homeView, contactView, faqView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -24,11 +25,6 @@ func contact(w http.ResponseWriter, r *http.Request) {
 func faq(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(faqView.Render(w, nil))
-}
-
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
 }
 
 func pageNotFound(w http.ResponseWriter, r *http.Request) {
@@ -48,14 +44,14 @@ func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	faqView = views.NewView("bootstrap", "views/faq.gohtml")
-	signupView = views.NewView("bootstrap", "views/signup.gohtml")
+	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
 	r.NotFoundHandler = nf
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/signup", usersC.New)
 
 	fmt.Println("Server listening on port 3000")
 	http.ListenAndServe(":3000", r)
