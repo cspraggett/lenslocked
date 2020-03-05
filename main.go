15ac954cdd8c2856b750ba6e565a663e4350cb7b
cspraggett <cspraggett@gmail.com>
2020-03-04 22:51:34 -0500

    clean up how views are made

--- main.go
+++ main.go
@@ -4,52 +4,19 @@ import (
 	"fmt"
 	"net/http"
 
-	// "github.com/julienschmidt/httprouter"
 	"github.com/gorilla/mux"
+
 	"lenslocked.com/controllers"
-	"lenslocked.com/views"
 )
 
-var homeView, contactView, faqView *views.View
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(faqView.Render(w, nil))
-}
-
-func pageNotFound(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>***404 Page Not Found!***</h1>")
-}
-
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-var nf http.Handler = http.HandlerFunc(pageNotFound)
-
 func main() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	faqView = views.NewView("bootstrap", "views/faq.gohtml")
+	staticC := controllers.NewStatic()
 	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
-	r.NotFoundHandler = nf
-	r.HandleFunc("/", home).Methods("GET")
-	r.HandleFunc("/contact", contact).Methods("GET")
-	r.HandleFunc("/faq", faq).Methods("GET")
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
+	r.Handle("/faq", staticC.Faq).Methods("GET")
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 
